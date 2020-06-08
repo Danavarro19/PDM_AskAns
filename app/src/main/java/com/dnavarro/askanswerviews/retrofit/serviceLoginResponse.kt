3a183596d1650c3a3f29dfc7a21e6872f0c08a78
GerryMG <gerardo.andresz4@live.com>
2020-06-08 01:26:07 -0600

    https

--- app/src/main/java/com/dnavarro/askanswerviews/retrofit/serviceLoginResponse.kt
+++ app/src/main/java/com/dnavarro/askanswerviews/retrofit/serviceLoginResponse.kt
@@ -3,6 +3,7 @@ package com.dnavarro.askanswerviews.retrofit
 import android.app.Application
 import android.content.Context
 import com.dnavarro.askanswerviews.MainActivity
+import com.dnavarro.askanswerviews.viewmodels.Userviewmodel
 import com.franmontiel.persistentcookiejar.ClearableCookieJar
 import com.franmontiel.persistentcookiejar.PersistentCookieJar
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache
@@ -15,14 +16,22 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.net.CookieHandler
 import java.net.CookieManager
 import java.net.CookiePolicy
 import java.net.CookieStore
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 object serviceLoginResponse {
 //    USE YOUR URL CHANGE IT
-    private const val URL: String = "http://192.168.1.6:3001/movil/"
+    private const val URL: String = "https://192.168.1.6:3001/movil/"
     private lateinit var context: Application
 
 
@@ -31,7 +40,69 @@ object serviceLoginResponse {
         storeC = PersistentCookieJar(SetCookieCache(), SharedPrefsCookiePersistor(this.context.applicationContext))
         intercep = HttpLoggingInterceptor()
         intercep.level = HttpLoggingInterceptor.Level.BODY
-        client = OkHttpClient.Builder().addNetworkInterceptor(intercep).retryOnConnectionFailure(true).cookieJar(storeC).build()
+
+        val Nullh: HostnameVerifier = object: HostnameVerifier {
+            override fun verify(hostname: String?, session: SSLSession?): Boolean {
+                println("hostName: $hostname")
+                println("session: $session")
+                return hostname == "192.168.1.6"
+            }
+        }
+
+        val xma: X509TrustManager = object: X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                println("auth type: " + authType)
+                chain!!.forEach {
+                    println(it)
+                }
+
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+                println("auth type: " + authType)
+                chain!!.forEach {
+                    println(it)
+                }
+
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+
+                return emptyArray()
+            }
+        }
+
+        val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+        println("directorio: " + File("cert.pem").getAbsolutePath())
+        val caInput: InputStream = BufferedInputStream(context.assets.open("cert.pem"))
+        val ca: X509Certificate = caInput.use {
+            cf.generateCertificate(it) as X509Certificate
+        }
+        System.out.println("ca=" + ca.subjectDN)
+
+        // Create a KeyStore containing our trusted CAs
+        val keyStoreType = KeyStore.getDefaultType()
+        val keyStore = KeyStore.getInstance(keyStoreType).apply {
+            load(null, null)
+            setCertificateEntry("ca", ca)
+        }
+
+        // Create a TrustManager that trusts the CAs inputStream our KeyStore
+        val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
+        val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
+            init(keyStore)
+        }
+
+        // Create an SSLContext that uses our TrustManager
+        val context2: SSLContext = SSLContext.getInstance("TLS").apply {
+            init(null, tmf.trustManagers, null)
+        }
+
+        client = OkHttpClient.Builder().addNetworkInterceptor(intercep).sslSocketFactory(context2.socketFactory,xma)
+            .retryOnConnectionFailure(true).cookieJar(storeC).hostnameVerifier(Nullh).build()
+
+
         retrofit = Retrofit.Builder()
 
             .baseUrl(URL)
